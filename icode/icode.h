3941dfe0cdaac8a6fce74b3ebb46fe81920ee8a3
Azeddine EL BASSBASI <elbassbasi@gmail.com>
2021-03-28 14:08:10 +0100

    first commit

---
+++ icode/icode.h
@@ -0,0 +1,11 @@
+/*
+ * icode.h
+ *
+ *  Created on: 1 déc. 2020
+ *      Author: azeddine
+ */
+
+#ifndef ICODE_ICODE_H_
+#define ICODE_ICODE_H_
+#include "include/icode.h"
+#endif /* ICODE_ICODE_H_ */
